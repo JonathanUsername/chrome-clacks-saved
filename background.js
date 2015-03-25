2a2caf1aaf47470a8b8803a7cff8667891997351
Jonathan King <jonathan.j.king@gmail.com>
2015-03-25 21:47:08 +0000

    housekeeping, some styling and reliable list fetching. Also clearing.

--- background.js
+++ background.js
@@ -14,12 +14,24 @@ function getList() {
     return list;
 };
 
+function clearList() {
+    chrome.storage.sync.remove("sites")
+    updateList()
+};
+
 // to get host from url
 var getLocation = function(href) {
     var l = document.createElement("a");
     l.href = href;
     return l;
 };
+
+var updateList = function(){
+    console.log("updateList")
+    chrome.storage.sync.get("sites", function(sites_data){ 
+        list = sites_data["sites"];
+    })
+}
 
 // The main listener to check each request's headers for clacks
 chrome.webRequest.onCompleted.addListener(
@@ -47,6 +59,7 @@ chrome.webRequest.onCompleted.addListener(
                 // Note from Pete: I've change += to just = to stop it repeating itself.
                 // - related to premature deletion? - don't think so...
                 clacks[details.tabId] = newClacks;
+                updateList()
                 chrome.pageAction.show[details.tabId];
                 if (DEBUG) console.log("store");
             }
@@ -70,41 +83,37 @@ chrome.webNavigation.onCommitted.addListener(
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         var tabId = sender.tab.id;
-        console.log(sender.tab)
 
         if (request.clacks) {
             if (clacks[tabId]) clacks[tabId] += "\n" + request.clacks;
             else clacks[tabId] = request.clacks;
         }
-
         // if there is a clacks entry for the loaded tab, show icon for that tab.
         if (clacks[tabId]){
             chrome.pageAction.show(tabId);
-            // My messy hack that saves the sites.
+            // My addition that saves the sites:
             if (SAVE){
+                // Just save the hostname
                 var url = getLocation(sender.tab.url).hostname
                 var saved = {
                     "sites": []
                 }
-                console.log(url)
+                // Get it from sync storage - this means it's saved across your account. Should make this an option in settings.
                 chrome.storage.sync.get("sites", function(sites_data){
                     if (sites_data){
-                        list = sites_data["sites"]
-                        // console.log("grabbed",sites_data)
-                        for (var i in sites_data["sites"]){
-                            saved.sites.push(sites_data["sites"][i])
+                        console.log("1",sites_data)
+                        var sites = sites_data["sites"]
+                        list = sites
+                        for (var i in sites){
+                            saved.sites.push(sites[i])
                         }
                     }
-                    // console.log(saved)
                     if (saved.sites.indexOf(url) == -1){
-                        console.log("saving",url)
                         saved.sites.push(url)
+                        // Save it to storage
                         chrome.storage.sync.set(saved, function(saved_data){
-                            // console.log("saved?",saved_data)
-                            chrome.storage.local.get("sites", function(sites_data){ 
-                                // console.log(sites_data);
-                                list = sites_data["sites"];
-                            })
+                            // Update the list
+                            updateList()
                         })
                     }
                 })
--- popup.js
+++ popup.js
@@ -1,6 +1,39 @@
 var tabId,
     clacks;
 
+clear_btn = document.getElementById("clearbtn");
+list_btn = document.getElementById("listbtn");
+list_elem = document.getElementById("list");
+count_elem = document.getElementById("count");
+
+document.getElementById("listbtn").addEventListener("click", function(){
+    chrome.runtime.getBackgroundPage(
+        function(bg) {
+            list_elem.innerHTML = ""
+            list = bg.getList();
+            counter = 0
+            for (var i in list){
+                counter++
+                list_elem.innerHTML += "<li>" + list[i] + "</li>"
+            }
+            count_elem.innerHTML = "Clacks towers visited: " + counter
+            list_btn.style.display = "none"
+            clear_btn.style.display = "block"
+            count_elem.style.display = "block"
+        }
+    );
+});
+
+document.getElementById("clearbtn").addEventListener("click", function(){
+    chrome.runtime.getBackgroundPage(
+        function(bg) {
+            bg.clearList();
+            list_elem.style.display = "none"
+            clear_btn.style.display = "none"
+        }
+    );
+})
+
 // Get the currently active tab in the focused window.
 chrome.tabs.query(
     {active: true, lastFocusedWindow: true},
@@ -11,14 +44,8 @@ chrome.tabs.query(
         chrome.runtime.getBackgroundPage(
             function(bg) {
                 clacks = bg.getClacks(tabId);
-                list = bg.getList();
                 // put text in popup.html
                 document.getElementById("text").textContent = clacks;
-                list_elem = document.getElementById("list")
-                list_elem.innerHTML = ""
-                for (var i in list){
-                    list_elem.innerHTML += "<li>" + list[i] + "</li>"
-                }
             }
         );
     }
